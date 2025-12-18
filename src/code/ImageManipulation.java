efee506ed6b49fc2f816fc32bd29515b644f087b
oAquatic <vincentjunze@foxmail.com>
2025-12-18 20:32:41 +0800

    Lab 9 completed! However, the given instructions for Challenge 3 were wrong. Rather than (left || bottom), it should be ((left && right) || (top && bottom)) to determine if a pixel should be set as black or white.

--- src/code/ImageManipulation.java
+++ src/code/ImageManipulation.java
@@ -1,95 +1,213 @@
 package code;
 
+import image.APImage;
 import image.Pixel;
 
 public class ImageManipulation {
 
-    /** CHALLENGE 0: Display Image
-     *  Write a statement that will display the image in a window
+    /**
+     * CHALLENGE 0: Display Image
+     * Write a statement that will display the image in a window
      */
     public static void main(String[] args) {
-
-
+        APImage image = new APImage("cyberpunk2077.jpg");
+        image.draw();
+        grayScale("cyberpunk2077.jpg");
+        blackAndWhite("cyberpunk2077.jpg");
+        edgeDetection("cyberpunk2077.jpg", 20);
+        reflectImage("cyberpunk2077.jpg");
+        rotateImage("cyberpunk2077.jpg");
     }
 
-    /** CHALLENGE ONE: Grayscale
+    /**
+     * CHALLENGE ONE: Grayscale
      *
      * INPUT: the complete path file name of the image
      * OUTPUT: a grayscale copy of the image
      *
-     * To convert a colour image to grayscale, we need to visit every pixel in the image ...
+     * To convert a colour image to grayscale, we need to visit every pixel in the
+     * image ...
      * Calculate the average of the red, green, and blue components of the pixel.
-     * Set the red, green, and blue components to this average value. */
-    public static void grayScale(String pathOfFile) {
-
+     * Set the red, green, and blue components to this average value.
+     */
+    public static void grayScale(String pathToFile) {
+        APImage image = new APImage(pathToFile);
+        for (Pixel p : image) {
+            int avg = getAverageColour(p);
+            setUniformColour(p, avg);
+        }
+        image.draw();
     }
 
-    /** A helper method that can be used to assist you in each challenge.
-     * This method simply calculates the average of the RGB values of a single pixel.
+    /**
+     * A helper method that can be used to assist you in each challenge.
+     * This method simply calculates the average of the RGB values of a single
+     * pixel.
+     * 
      * @param pixel
      * @return the average RGB value
      */
     private static int getAverageColour(Pixel pixel) {
-        return 0;
+        int red = pixel.getRed();
+        int green = pixel.getBlue();
+        int blue = pixel.getGreen();
+        return (red + green + blue) / 3;
     }
 
-    /** CHALLENGE TWO: Black and White
+    private static void setUniformColour(Pixel pixel, int value) {
+        pixel.setRed(value);
+        pixel.setGreen(value);
+        pixel.setBlue(value);
+    }
+
+    /**
+     * CHALLENGE TWO: Black and White
      *
      * INPUT: the complete path file name of the image
      * OUTPUT: a black and white copy of the image
      *
-     * To convert a colour image to black and white, we need to visit every pixel in the image ...
+     * To convert a colour image to black and white, we need to visit every pixel in
+     * the image ...
      * Calculate the average of the red, green, and blue components of the pixel.
      * If the average is less than 128, set the pixel to black
-     * If the average is equal to or greater than 128, set the pixel to white */
-    public static void blackAndWhite(String pathOfFile) {
-
+     * If the average is equal to or greater than 128, set the pixel to white
+     */
+    public static void blackAndWhite(String pathToFile) {
+        APImage image = new APImage(pathToFile);
+        for (Pixel p : image) {
+            int avg = getAverageColour(p);
+            if (avg < 128) {
+                setUniformColour(p, 0);
+            }
+            if (avg >= 128) {
+                setUniformColour(p, 255);
+            }
+        }
+        image.draw();
     }
 
-    /** CHALLENGE Three: Edge Detection
+    /**
+     * CHALLENGE Three: Edge Detection
      *
      * INPUT: the complete path file name of the image
-     * OUTPUT: an outline of the image. The amount of information will correspond to the threshold.
+     * OUTPUT: an outline of the image. The amount of information will correspond to
+     * the threshold.
      *
-     * Edge detection is an image processing technique for finding the boundaries of objects within images.
-     * It works by detecting discontinuities in brightness. Edge detection is used for image segmentation
-     * and data extraction in areas such as image processing, computer vision, and machine vision.
+     * Edge detection is an image processing technique for finding the boundaries of
+     * objects within images.
+     * It works by detecting discontinuities in brightness. Edge detection is used
+     * for image segmentation
+     * and data extraction in areas such as image processing, computer vision, and
+     * machine vision.
      *
-     * There are many different edge detection algorithms. We will use a basic edge detection technique
+     * There are many different edge detection algorithms. We will use a basic edge
+     * detection technique
      * For each pixel, we will calculate ...
      * 1. The average colour value of the current pixel
-     * 2. The average colour value of the pixel to the left of the current pixel
-     * 3. The average colour value of the pixel below the current pixel
-     * If the difference between 1. and 2. OR if the difference between 1. and 3. is greater than some threshold value,
-     * we will set the current pixel to black. This is because an absolute difference that is greater than our threshold
+     * 2. The average colour VALUES of the pixel to the left of ****AND RIGHT
+     * OF!!!**** the
+     * current pixel
+     * 3. The average colour VALUES of the pixel below ****AND ABOVE!!!**** the
+     * current
+     * pixel
+     * If the difference between 1. and 2. OR if the difference between 1. and 3. is
+     * greater than some threshold value,
+     * we will set the current pixel to black. This is because an absolute
+     * difference that is greater than our threshold
      * value should indicate an edge and thus, we colour the pixel black.
      * Otherwise, we will set the current pixel to white
      * NOTE: We want to be able to apply edge detection using various thresholds
-     * For example, we could apply edge detection to an image using a threshold of 20 OR we could apply
+     * For example, we could apply edge detection to an image using a threshold of
+     * 20 OR we could apply
      * edge detection to an image using a threshold of 35
-     *  */
+     */
     public static void edgeDetection(String pathToFile, int threshold) {
-
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        boolean horizontal = false;
+        boolean vertical = false;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Pixel p = image.getPixel(x, y);
+                if (x > 0 && x < width - 1) {
+                    Pixel pL = image.getPixel(x - 1, y);
+                    Pixel pR = image.getPixel(x + 1, y);
+                    if (Math.abs(getAverageColour(pL)
+                            - getAverageColour(p)) > threshold
+                            && Math.abs(getAverageColour(pR) - getAverageColour(p)) > threshold) {
+                        horizontal = true;
+                    } else {
+                        horizontal = false;
+                    }
+                }
+                if (y < height - 1 && y > 0) {
+                    Pixel pB = image.getPixel(x, y + 1);
+                    Pixel pU = image.getPixel(x, y - 1);
+                    if (Math.abs(getAverageColour(pB)
+                            - getAverageColour(p)) > threshold
+                            && Math.abs(getAverageColour(pU) - getAverageColour(p)) > threshold) {
+                        vertical = true;
+                    } else {
+                        vertical = false;
+                    }
+                }
+                if (horizontal || vertical) {
+                    setUniformColour(p, 0);
+                } else {
+                    setUniformColour(p, 255);
+                }
+            }
+        }
+        image.draw();
     }
 
-    /** CHALLENGE Four: Reflect Image
+    /**
+     * CHALLENGE Four: Reflect Image
      *
      * INPUT: the complete path file name of the image
      * OUTPUT: the image reflected about the y-axis
      *
      */
     public static void reflectImage(String pathToFile) {
-
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width / 2; x++) {
+                Pixel p = image.getPixel(x, y);
+                Pixel pFlip = image.getPixel(width - 1 - x, y);
+                image.setPixel(width - 1 - x, y, p);
+                image.setPixel(x, y, pFlip);
+            }
+        }
+        image.draw();
     }
 
-    /** CHALLENGE Five: Rotate Image
+    /**
+     * CHALLENGE Five: Rotate Image
      *
      * INPUT: the complete path file name of the image
      * OUTPUT: the image rotated 90 degrees CLOCKWISE
      *
-     *  */
+     */
     public static void rotateImage(String pathToFile) {
-
+        APImage image = new APImage(pathToFile);
+        int width = image.getWidth();
+        int height = image.getHeight();
+        APImage rotatedImage = new APImage(height, width);
+        int posX = height - 1;
+        int posY = 0;
+        for (int y = 0; y < height; y++) {
+            posY = 0;
+            for (int x = 0; x < width; x++) {
+                Pixel p = image.getPixel(x, y);
+                rotatedImage.setPixel(posX, posY, p);
+                posY++;
+            }
+            posX--;
+        }
+        rotatedImage.draw();
     }
 
 }
--- src/image/APImage.java
+++ src/image/APImage.java
@@ -9,65 +9,78 @@ import java.util.*;
 
 /**
  * LICENSE: This is open-source software released under the terms of the<BR>
- * GPL (http://www.gnu.org/licenses/gpl.html).<BR><BR> 
+ * GPL (http://www.gnu.org/licenses/gpl.html).<BR>
+ * <BR>
  *
- * This class supports the processing of images.<BR><BR>  
+ * This class supports the processing of images.<BR>
+ * <BR>
  * 
- * Input image files must be in JPEG format.  Output files are written 
- * in JPEG format.<BR><BR>
+ * Input image files must be in JPEG format. Output files are written
+ * in JPEG format.<BR>
+ * <BR>
  *
  * Example program, which loads an image from a file dialog,
- * converts it to grayscale, and draws it:<BR><BR>
+ * converts it to grayscale, and draws it:<BR>
+ * <BR>
  * 
- *<pre> import images.APImage;
+ * <pre>
+ * import images.APImage;
  * import images.Pixel;
  *
- * public class TestGrayscale{
+ * public class TestGrayscale {
  *
- *    public static void main(String[]args){  
- *       APImage image = new APImage();
- *       for (Pixel p : image){
- *          int red = p.getRed();
- *          int green = p.getGreen();
- *          int blue = p.getBlue();
- *          int average = (red + green + blue) / 3;
- *          p.setRed(average);
- *          p.setGreen(average);
- *          p.setBlue(average);
- *       }
- *       image.draw();
- *    }
- * }</pre><BR> 
- * You can reset maximum heap size when running the program if you run 
+ *     public static void main(String[] args) {
+ *         APImage image = new APImage();
+ *         for (Pixel p : image) {
+ *             int red = p.getRed();
+ *             int green = p.getGreen();
+ *             int blue = p.getBlue();
+ *             int average = (red + green + blue) / 3;
+ *             p.setRed(average);
+ *             p.setGreen(average);
+ *             p.setBlue(average);
+ *         }
+ *         image.draw();
+ *     }
+ * }
+ * </pre>
+ * 
+ * <BR>
+ * You can reset maximum heap size when running the program if you run
  * out of memory for large images, as follows:<BR>
  *
- * <pre>java -Xmx<I>new Size</I>m <I>programFileName</I></pre>
+ * <pre>
+ * java -Xmx<I>new Size</I>m <I>programFileName</I>
+ * </pre>
  *
- * where <I>newSize</I> is an integer.  For example,<BR>
+ * where <I>newSize</I> is an integer. For example,<BR>
  *
- * <pre>java -Xmx128m TestGrayscale</pre>
+ * <pre>
+ * java -Xmx128m TestGrayscale
+ * </pre>
  *
  * allows up to 128 megabytes of heap space for the TestGrayscale program.
-*/
+ */
 
-public class APImage extends JFrame implements Iterable<Pixel>{
-   
+public class APImage extends JFrame implements Iterable<Pixel> {
+
     // Default width and height of blank images
     static private final int WIDTH = 200;
     static private final int HEIGHT = 200;
-   
+
     // Instance variables
-    private Pixel[] pixels;          // Array of pixels
-    private Image image;             // The bitmap for display
-    private ImagePanel imagePanel;   // The image's panel in its window
-    private String fileName;         // The file name or empty if no file yet
+    private Pixel[] pixels; // Array of pixels
+    private Image image; // The bitmap for display
+    private ImagePanel imagePanel; // The image's panel in its window
+    private String fileName; // The file name or empty if no file yet
 
     /**
      * Creates a blank image whose color is black.
-     * @param width the width of the image in pixels
+     * 
+     * @param width  the width of the image in pixels
      * @param height the height of the image in pixels
      */
-    public APImage(int width, int height){
+    public APImage(int width, int height) {
         fileName = "";
         image = createImage(getBlankImage(width, height));
         setWindowAttributes();
@@ -75,36 +88,37 @@ public class APImage extends JFrame implements Iterable<Pixel>{
 
     /**
      * Creates an image from a given file.
+     * 
      * @param fileName the name of the file
      * @throws Exception if the file does not exist or is not in JPEG format
      */
-    public APImage(String fileName){
+    public APImage(String fileName) {
         this.fileName = fileName;
         image = getImageFromFile(fileName);
         setWindowAttributes();
     }
 
     /**
-     * Creates an image from a file dialog selection or creates a blank image if the user cancels.
+     * Creates an image from a file dialog selection or creates a blank image if the
+     * user cancels.
      */
-    public APImage(){
+    public APImage() {
         File file = openFileDialog();
-        if (file == null){
+        if (file == null) {
             fileName = "";
             image = createImage(getBlankImage(WIDTH, HEIGHT));
-	    }
-	    else{
+        } else {
             fileName = file.getName();
             image = getImageFromFile(fileName);
-	    }
-	    setWindowAttributes();
+        }
+        setWindowAttributes();
     }
-   
+
     // Creates a copy of the given image
-    private APImage(APImage original){
+    private APImage(APImage original) {
         this(original.getWidth(), original.getHeight());
         for (int x = 0; x < original.getWidth(); x++)
-            for (int y = 0; y < original.getHeight(); y++){
+            for (int y = 0; y < original.getHeight(); y++) {
                 Pixel p1 = original.getPixel(x, y);
                 Pixel p2 = this.getPixel(x, y);
                 p2.setRed(p1.getRed());
@@ -115,85 +129,93 @@ public class APImage extends JFrame implements Iterable<Pixel>{
 
     /**
      * Returns the width of the image in pixels.
+     * 
      * @return the image's width in pixels
      */
-    public int getWidth(){
+    public int getWidth() {
         return image.getWidth(this);
     }
 
     /**
      * Returns the height of the image in pixels.
+     * 
      * @return the image's height in pixels
      */
-    public int getHeight(){
+    public int getHeight() {
         return image.getHeight(this);
     }
 
     /**
      * Returns the pixel at the given (x, y) position.
+     * 
      * @param x the column position of the pixel
      * @param y the row position of the pixel
      * @return the Pixel at the given (x, y) position
      */
-    public Pixel getPixel(int x, int y){
+    public Pixel getPixel(int x, int y) {
         return pixels[y * image.getWidth(this) + x];
     }
 
     /**
      * Resets the pixel at the given (x, y) position but does not redraw it.
+     * 
      * @param x the column position of the pixel
      * @param y the row position of the pixel
      * @param p the new Pixel
-     */ 
-    public void setPixel(int x, int y, Pixel p){
+     */
+    public void setPixel(int x, int y, Pixel p) {
         pixels[y * image.getWidth(this) + x] = p;
     }
-   
+
     /**
      * Displays the image in its window.
-     */ 
-    public void draw(){
+     */
+    public void draw() {
         setVisible(true);
         updateImage();
-    } 
+    }
 
     /**
      * Returns a copy of the image.
+     * 
      * @return an APImage representing a copy of this image
      */
-    public APImage clone(){
+    public APImage clone() {
         return new APImage(this);
     }
 
     /**
      * Returns a string representation of the image (file name, width, and height).
+     * 
      * @return a string representation of the image
      */
-    public String toString(){
-       if (image == null)
-           return null;
-       else
-           return "IMAGE\n" + 
-                  "File name: " + fileName + "\n" +
-                  "Width: " + getWidth() + "\n" +
-                  "Height: " + getHeight();
+    public String toString() {
+        if (image == null)
+            return null;
+        else
+            return "IMAGE\n" +
+                    "File name: " + fileName + "\n" +
+                    "Width: " + getWidth() + "\n" +
+                    "Height: " + getHeight();
     }
 
     /**
      * Returns an iterator on the image.
+     * 
      * @return an Iterator on the image
      */
-    public Iterator<Pixel> iterator(){
+    public Iterator<Pixel> iterator() {
         return new ImageIterator();
     }
 
     /**
-     * Saves the image under its current file name or runs <code>saveAs</code> if no file name yet.
+     * Saves the image under its current file name or runs <code>saveAs</code> if no
+     * file name yet.
      */
-    public boolean save(){
-        if (fileName.equals("")) 
+    public boolean save() {
+        if (fileName.equals(""))
             return saveAs();
-        try{
+        try {
             int w = image.getWidth(APImage.this);
             int h = image.getHeight(APImage.this);
             int[] pix = pixelsToInts(pixels);
@@ -201,7 +223,7 @@ public class APImage extends JFrame implements Iterable<Pixel>{
             buffer.setRGB(0, 0, w, h, pix, 0, w);
             File file = new File(fileName);
             ImageIO.write(buffer, "jpg", file);
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err.println(e);
             return false;
         }
@@ -210,9 +232,10 @@ public class APImage extends JFrame implements Iterable<Pixel>{
 
     /**
      * Saves the image using a file dialog.
+     * 
      * @return true if the save is successful, or false if the user cancels
      */
-    public boolean saveAs(){
+    public boolean saveAs() {
         File file = saveFileDialog();
         if (file == null)
             return false;
@@ -222,20 +245,20 @@ public class APImage extends JFrame implements Iterable<Pixel>{
     }
 
     // Sets the image window's attributes
-    private void setWindowAttributes(){
+    private void setWindowAttributes() {
         setTitle(fileName);
-        //setResizable(false);
-        imagePanel = new ImagePanel(image, 
-                                    image.getWidth(this), 
-                                    image.getHeight(this));
+        // setResizable(false);
+        imagePanel = new ImagePanel(image,
+                image.getWidth(this),
+                image.getHeight(this));
         Container c = getContentPane();
         c.setBackground(Color.white);
         c.add(imagePanel);
         pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-   
-    private void updateImage(){
+
+    private void updateImage() {
         int w = image.getWidth(APImage.this);
         int h = image.getHeight(APImage.this);
         image = createImage(new MemoryImageSource(w, h, pixelsToInts(pixels), 0, w));
@@ -243,26 +266,26 @@ public class APImage extends JFrame implements Iterable<Pixel>{
     }
 
     // Maps the image into the array of pixels
-    private Pixel[] imageToPixels(Image image){
+    private Pixel[] imageToPixels(Image image) {
         int w = image.getWidth(this);
         int h = image.getHeight(this);
         int[] pixels = new int[w * h];
-        PixelGrabber pg = new PixelGrabber(image, 0, 0, w, h, pixels, 0, w); 
-        try{
+        PixelGrabber pg = new PixelGrabber(image, 0, 0, w, h, pixels, 0, w);
+        try {
             pg.grabPixels();
-        }catch (InterruptedException e2) {
+        } catch (InterruptedException e2) {
             System.err.println("Interrupted waiting for pixels!");
             return null;
-	}
+        }
         if ((pg.getStatus() & ImageObserver.ABORT) != 0) {
             System.err.println("Image fetch aborted or errored");
-	    return null;
+            return null;
         }
         return intsToPixels(pixels);
     }
 
     // Maps an array of int to an array of pixels
-    private Pixel[] intsToPixels(int[] pixels){
+    private Pixel[] intsToPixels(int[] pixels) {
         Pixel[] pix = new Pixel[pixels.length];
         for (int i = 0; i < pixels.length; i++)
             pix[i] = new Pixel(pixels[i]);
@@ -270,7 +293,7 @@ public class APImage extends JFrame implements Iterable<Pixel>{
     }
 
     // Maps an array of pixels to an array of int
-    private int[] pixelsToInts(Pixel[] pixels){
+    private int[] pixelsToInts(Pixel[] pixels) {
         int[] pix = new int[pixels.length];
         for (int i = 0; i < pixels.length; i++)
             pix[i] = pixels[i].getValue();
@@ -278,7 +301,7 @@ public class APImage extends JFrame implements Iterable<Pixel>{
     }
 
     // Creates an image of width w and height h with black pixels
-    private ImageProducer getBlankImage(int w, int h){
+    private ImageProducer getBlankImage(int w, int h) {
         pixels = new Pixel[w * h];
         for (int i = 0; i < pixels.length; i++)
             pixels[i] = new Pixel(0, 0, 0);
@@ -286,72 +309,72 @@ public class APImage extends JFrame implements Iterable<Pixel>{
     }
 
     // Reads an image from a file and updates the pixels array
-    private Image getImageFromFile(String fileName){
+    private Image getImageFromFile(String fileName) {
         Image image = null;
-        try{
+        try {
             File file = new File(fileName);
             image = ImageIO.read(file);
-            while (image.getWidth(APImage.this) < 0)  // Wait for size to be known
-                {}
+            while (image.getWidth(APImage.this) < 0) // Wait for size to be known
+            {
+            }
             pixels = imageToPixels(image);
-        }catch(Exception e){
-            System.out.println(e); 
+        } catch (Exception e) {
+            System.out.println(e);
         }
         return image;
     }
-   
-    private File openFileDialog(){
+
+    private File openFileDialog() {
         JFileChooser chooser = new JFileChooser();
         chooser.setFileFilter(new ImageFileFilter(".jpg", "JPEG images (*.jpg)"));
         int result = chooser.showOpenDialog(null);
         if (result == JFileChooser.CANCEL_OPTION)
             return null;
         else
-            try{
+            try {
                 File file = chooser.getSelectedFile();
                 return file;
-            }catch(Exception e){
+            } catch (Exception e) {
                 JOptionPane.showMessageDialog(null,
-                                              e.toString());
+                        e.toString());
             }
-            return null;
+        return null;
     }
 
-    private File saveFileDialog(){
+    private File saveFileDialog() {
         JFileChooser chooser = new JFileChooser();
         chooser.setFileFilter(new ImageFileFilter(".jpg", "JPEG images (*.jpg)"));
         int result = chooser.showSaveDialog(null);
         if (result == JFileChooser.CANCEL_OPTION)
             return null;
         else
-            try{
+            try {
                 File file = chooser.getSelectedFile();
                 return file;
-            }catch(Exception e){
+            } catch (Exception e) {
                 JOptionPane.showMessageDialog(null,
-                                              e.toString());
+                        e.toString());
             }
-            return null;
+        return null;
     }
-    
-    private class ImageIterator implements Iterator<Pixel>{
+
+    private class ImageIterator implements Iterator<Pixel> {
         private int pos = 0;
-      
-        public boolean hasNext(){
+
+        public boolean hasNext() {
             return pos < pixels.length;
         }
 
-        public Pixel next(){
-           if (! hasNext())
-               throw new NoSuchElementException();
-           pos++;
-           return pixels[pos - 1];
+        public Pixel next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            pos++;
+            return pixels[pos - 1];
         }
 
-        public void remove(){
+        public void remove() {
             throw new UnsupportedOperationException();
         }
     }
-       
+
 }
-      
